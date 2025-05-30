9a0877577b04a2ac9bea5809b17c4836a2fa498f
LehMipo412 <barbetzalel412@gmail.com>
2025-05-30 22:56:00 +0300

    added pre and post build callbacks + a dialogue in the end of each one

---
+++ Assets/Scripts/GameObjectRelated/DemiActor.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class DemiActor : MonoBehaviour, IResteable
+{
+    public void Reset()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
