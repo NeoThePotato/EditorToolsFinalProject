21679c2da2e021cdff8997a4dee2afbe603626ab
NeoThePotato <neorayb@gmail.com>
2025-06-12 02:22:44 +0300

    Add utility for making sure cameras aren't null-ref'ed.

---
+++ Assets/Scripts/Editor/SetAllCameraReferencesToMainCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+
+public static class SetAllCameraReferencesToMainCamera
+{
+	/// <summary>
+	/// Finds any <see cref="Component"/> which has a <see cref="Camera"/> field which is <see langword="null"/> and sets it to <see cref="Camera.main"/>, if exists.
+	/// </summary>
+	[MenuItem("Tools/Set All Camera References To MainCamera")]
+	public static void SetReferences()
+	{
+		var mainCamera = Camera.main;
+		if (!mainCamera)
+		{
+			Debug.LogWarning("No Camera tagged with \'MainCamera\'.");
+			return;
+		}
+		foreach (var component in Object.FindObjectsByType<Component>(FindObjectsSortMode.None))
+		{
+			// It's reflection-ing time!
+			foreach (var field in component.GetType().GetTypeInfo().GetFields().Where(IsNullCameraReference))
+			{
+				field.SetValue(component, mainCamera);
+				EditorUtility.SetDirty(component);
+				Debug.Log($"Set \'{component}.{field.Name}\' reference to \'{mainCamera}\'.");
+			}
+
+			bool IsNullCameraReference(FieldInfo fieldInfo) => fieldInfo.FieldType == typeof(Camera) && fieldInfo.GetValue(component) as Camera == null;
+		}
+	}
+}
