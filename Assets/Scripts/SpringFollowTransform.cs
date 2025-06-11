dd59d96481b0f78e48cb3dd45168bf5392afee71
NeoThePotato <neorayb@gmail.com>
2025-06-12 01:50:34 +0300

    Add components for spring-y follow.

---
+++ Assets/Scripts/SpringFollowTransform.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Unity.Mathematics;
+
+public class SpringFollowTransform : MonoBehaviour
+{
+	[SerializeField] private Spring.Parameters _springParameters;
+	[SerializeField] private Transform _toFollow;
+	private float3 _velocity;
+
+	private void Update()
+	{
+		float3 position = transform.position;
+		Spring.Apply(ref position, ref _velocity, _toFollow.position, in _springParameters, Time.deltaTime);
+		transform.position = position;
+	}
+}
