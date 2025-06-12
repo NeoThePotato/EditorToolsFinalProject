41c1bfa2ad222a267600a06b179c0b517301b400
NeoThePotato <neorayb@gmail.com>
2025-06-12 03:41:31 +0300

    Create a custom PropertyDrawer for 'CameraProjectionSkewSpring', the most evil struct in this codebase (So far).

---
+++ Assets/Scripts/Editor/CameraProjectionSkewDrawer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+using static CameraProjectionSkew;
+
+[CustomPropertyDrawer(typeof(CameraProjectionSkewSpring))]
+public class CameraProjectionSkewDrawer : PropertyDrawer
+{
+	private SerializedProperty springProperty;
+	private SerializedProperty sensitivityProperty;
+	private HelpBox noSensitivityWarningBox;
+
+	public override VisualElement CreatePropertyGUI(SerializedProperty property)
+	{
+		var container = new VisualElement();
+
+		// Values
+		springProperty = property.FindPropertyRelative("spring");
+		sensitivityProperty = property.FindPropertyRelative("sensitivity");
+		noSensitivityWarningBox = new HelpBox()
+		{
+			messageType = HelpBoxMessageType.Warning,
+			text = "\'CameraProjectionSkewSpring\' has 0 sensitivity, effects will not be felt.\nIf this is what you intend - it's recommended to turn off its controlling \'CameraProjectionSkew\' MonoBehaviour to save on performance."
+		};
+		var rigidnessField = new PropertyField(springProperty);
+		var dampingField = new PropertyField(sensitivityProperty);
+		container.Add(rigidnessField);
+		container.Add(dampingField);
+		container.Add(noSensitivityWarningBox);
+		UpdateWarning();
+		container.RegisterCallback<SerializedPropertyChangeEvent>(UpdateWarning);
+
+		return container;
+	}
+
+	private void UpdateWarning(SerializedPropertyChangeEvent _) => UpdateWarning();
+
+	private void UpdateWarning() => noSensitivityWarningBox.visible = sensitivityProperty.floatValue <= 0f;
+}
